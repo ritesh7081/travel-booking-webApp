4cde551c812ec456341a403e399ebe4362ef3179
Ritesh Kumar <riteshjnv9838@gmail.com>
2026-01-26 01:04:16 +0530

    Some problem in frontend

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -3,7 +3,7 @@
 
 const API_CONFIG = {
   // Development base URL (dummy URL for development)
-  BASE_URL:  'https://travel-booking-app-backend-e5ph.onrender.com/',
+  BASE_URL:  'https://travel-booking-app-backend-e5ph.onrender.com/api',
   
   
   // API Endpoints
